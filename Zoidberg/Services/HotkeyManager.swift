b3eb89efbbdf36aaf6bdce7551198f2cc76de66a
Alex Mathers <alex.mathers@wealthsimple.com>
2026-03-13 15:50:55 -0700

    feat: add HotkeyManager with global Ctrl+Space and Ctrl+Shift+Space shortcuts

---
+++ Zoidberg/Services/HotkeyManager.swift
@@ -0,0 +1,28 @@
+// Zoidberg/Services/HotkeyManager.swift
+import HotKey
+import Carbon
+
+final class HotkeyManager {
+    private var togglePanelHotKey: HotKey?
+    private var dictateHotKey: HotKey?
+
+    var onTogglePanel: (() -> Void)?
+    var onToggleDictation: (() -> Void)?
+
+    func register() {
+        togglePanelHotKey = HotKey(key: .space, modifiers: [.control])
+        togglePanelHotKey?.keyDownHandler = { [weak self] in
+            self?.onTogglePanel?()
+        }
+
+        dictateHotKey = HotKey(key: .space, modifiers: [.control, .shift])
+        dictateHotKey?.keyDownHandler = { [weak self] in
+            self?.onToggleDictation?()
+        }
+    }
+
+    func unregister() {
+        togglePanelHotKey = nil
+        dictateHotKey = nil
+    }
+}
